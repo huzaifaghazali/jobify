edc28044856403ade251aa76d0f385dac58e6b01
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-13 21:41:48 +0500

    Add register page layout

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,33 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
+import Wrapper from '../assets/wrappers/RegisterAndLogin';
+import FormRow from '../components/FormRow';
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
-  )
-}
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
 
-export default Register
+        <FormRow type='text' name='name' />
+        <FormRow type='text' name='lasName' labelText='Last Name' />
+        <FormRow type='text' name='location' />
+        <FormRow type='email' name='email' />
+        <FormRow type='password' name='password' />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <p>
+          Already a member?
+          <Link to='/login' className='member-btn'>
+            Login Page
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Register;
