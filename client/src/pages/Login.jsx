c30e0791d426908a2e74d6c63e03002c1744c591
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-13 21:46:46 +0500

    Add login page ui

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
+import Logo from '../components/Logo';
+import FormRow from '../components/FormRow';
+import Wrapper from '../assets/wrappers/RegisterAndLogin';
 
 const Login = () => {
   return (
-    <div>
-       <h1>Login</h1>
-       <Link to='/register'>Register Page</Link>
-    </div>
-  )
-}
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Login</h4>
 
-export default Login
+        <FormRow type='email' name='email' defaultValue='test@gmail.com' />
+        <FormRow type='password' name='password' defaultValue='secret123' />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+        <button type='button' className='btn btn-block'>
+          explore the app
+        </button>
+
+        <p>
+          Not a member yet?
+          <Link to='/register' className='member-btn'>
+            Register
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
+  );
+};
+
+export default Login;
