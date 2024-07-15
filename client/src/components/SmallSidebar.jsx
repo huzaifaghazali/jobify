3337ad1352ab58d36ecd37991fb94b3522e34775
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-15 20:12:39 +0500

    Add small sidebar component

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,6 +1,48 @@
+import { NavLink } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useDashboardContext } from '../pages/DashboardLayout';
+import links from '../utils/link';
+import Logo from './Logo';
 
 export default function SmallSidebar() {
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+
   return (
-    <div>SmallSidebar</div>
-  )
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>
+            {links.map((link) => {
+              const { text, path, icon } = link;
+
+              return (
+                <NavLink
+                  to={path}
+                  key={text}
+                  className='nav-link'
+                  onClick={toggleSidebar}
+                  end
+                >
+                  <span className='icon'>{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
