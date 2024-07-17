d1b96a82907eb86bbbde185d3ac3986684f398ff
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-17 12:21:49 +0500

    Add jobs controller

--- server/server.js
+++ server/server.js
@@ -11,15 +11,20 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-const port = process.env.PORT || 5100;
-
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.post('/', (req, res) => {
-  res.json({ message: 'Data received', data: req.body });
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: 'not found' });
 });
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: 'something went wrong' });
+});
+
+const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
   console.log('server running....');
