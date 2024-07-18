ddbee00a0a13aaea6bb244c00b27249114e3d691
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-18 13:47:00 +0500

    Add error middleware and custom error class

--- server/server.js
+++ server/server.js
@@ -5,7 +5,11 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 
+// routers
 import jobRouter from './routes/jobRouter.js';
+
+// middleware
+import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
 
 const app = express();
 
@@ -25,10 +29,7 @@ app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: 'something went wrong' });
-});
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
 
