22b02c4bf0448c661e421cb2194f68b8cda37231
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-18 13:37:31 +0500

    Update jobs controller

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,50 +1,52 @@
 import Job from '../models/JobModel.js';
+import { StatusCodes } from 'http-status-codes';
 
 export const getAllJobs = async (req, res) => {
   const jobs = await Job.find({});
-  res.status(200).json({ jobs });
+  res.status(StatusCodes.OK).json({ jobs });
 };
 
 export const getJob = async (req, res) => {
   const { id } = req.params;
   const job = await Job.findById(id);
   if (!job) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no job with id ${id}` });
   }
-  res.status(200).json({ job });
+  res.status(StatusCodes.OK).json({ job });
 };
 
 export const createJob = async (req, res) => {
   const { company, position } = req.body;
 
-  if (!company || !position) {
-    return res.status(400).json({ msg: 'Please provide company and position' });
-  }
-
   const job = await Job.create({ company, position });
-  res.status(201).json({ job });
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 export const updateJob = async (req, res) => {
   const { id } = req.params;
-  console.log(id);
 
   const updatedJob = await Job.findOneAndUpdate({ _id: id }, req.body, {
     new: true,
   });
 
   if (!updatedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no job with id ${id}` });
   }
 
-  res.status(200).json({ msg: 'job modified', job: updatedJob });
+  res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob });
 };
 
 export const deleteJob = async (req, res) => {
   const { id } = req.params;
   const removedJob = await Job.findOneAndDelete({ _id: id });
   if (!removedJob) {
-    return res.status(404).json({ msg: `no job with id ${id}` });
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `no job with id ${id}` });
   }
-  res.status(200).json({ msg: 'job deleted', job: removedJob });
+  res.status(StatusCodes.OK).json({ msg: 'job deleted', job: removedJob });
 };
