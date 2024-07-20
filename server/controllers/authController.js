c7626e09604a2878415a137fd3561366bd5ab9e1
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-20 20:40:58 +0500

    Add functionality to create a user

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,12 @@
-export const register = (req, res) => {
-  res.send('register');
+import { StatusCodes } from 'http-status-codes';
+import User from '../models/UserModel.js';
+
+export const register = async (req, res) => {
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? 'admin' : 'user';
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 export const login = (req, res) => {
--- server/middlewares/validationMiddleware.js
+++ server/middlewares/validationMiddleware.js
@@ -3,6 +3,7 @@ import { BadRequestError, NotFoundError } from '../errors/customErrors.js';
 import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 import mongoose from 'mongoose';
 import Job from '../models/JobModel.js';
+import User from '../models/UserModel.js';
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -40,4 +41,26 @@ export const validateIdParam = withValidationErrors([
     const job = await Job.findById(value);
     if (!job) throw new NotFoundError(`no job with id : ${value}`);
   }),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body('name').notEmpty().withMessage('Name is required'),
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email format')
+    .custom(async (value) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError('Email already in use');
+      }
+    }),
+  body('password')
+    .notEmpty()
+    .withMessage('Password is required')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters long'),
+  body('location').notEmpty().withMessage('location is required'),
+  body('lastName').notEmpty().withMessage('last name is required'),
 ]);
