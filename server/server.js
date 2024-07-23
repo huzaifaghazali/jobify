a2c54e35ae0775028fae615f84814ad12e29cf71
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-23 12:59:54 +0500

    Add simple user routes and controller

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 
 // routers
 import jobRouter from './routes/jobRouter.js';
+import userRouter from './routes/userRouter.js';
 import authRouter from './routes/authRouter.js';
 
 // middleware
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
 app.use('*', (req, res) => {
