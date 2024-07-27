f406acd6f907b6a83b67c7a8f082cc257ac646a3
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-27 21:48:48 +0500

    Add jobs container component

--- client/src/components/JobsContainer.jsx
+++ client/src/components/JobsContainer.jsx
@@ -1,8 +1,26 @@
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useAllJobsContext } from '../pages/AllJobs';
+import Job from './Job';
 
 const JobsContainer = () => {
+  const { data } = useAllJobsContext();
+  const { jobs } = data;
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display</h2>
+      </Wrapper>
+    );
+  }
   return (
-    <div>JobsContainer</div>
-  )
-}
+    <Wrapper>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
 
-export default JobsContainer
+export default JobsContainer;
