cae50a52db75c3587fca021340ac1299a8fd9547
huzaifaghazali <huzaifaghazali@gmail.com>
2024-07-30 22:05:29 +0500

    Add profile page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,55 @@
+import { useNavigation, Form } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+import { FormRow } from '../components';
+import customFetch from '../utils/customFetch';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user } = useOutletContext();
+  const { name, lastName, email, location } = user;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
 
-export default Profile
+  return (
+    <Wrapper>
+      <Form method='post' className='form' encType='multipart/form-data'>
+        <h4 className='form-title'>Profile</h4>
+
+        <div className='form-center'>
+          <div className='form-row'>
+            <label htmlFor='image' className='form-label'>
+              Select an image file (max 0.5 MB):
+            </label>
+            <input
+              type='file'
+              id='avatar'
+              name='avatar'
+              className='form-input'
+              accept='image/*'
+            />
+          </div>
+          <FormRow type='text' name='name' defaultValue={name} />
+          <FormRow
+            type='text'
+            labelText='last name'
+            name='lastName'
+            defaultValue={lastName}
+          />
+          <FormRow type='email' name='email' defaultValue={email} />
+          <FormRow type='text' name='location' defaultValue={location} />
+          <button
+            className='btn btn-block form-btn'
+            type='submit'
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? 'submitting...' : 'save changes'}
+          </button>
+        </div>
+      </Form>
+    </Wrapper>
+  );
+};
+
+export default Profile;
