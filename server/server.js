24d90bcea46b640d6575ca84cdadc703df10621e
huzaifaghazali <huzaifaghazali@gmail.com>
2024-08-06 12:25:06 +0500

    Update server code

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -44,6 +44,10 @@ app.get('/', (req, res) => {
 app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
 
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
