efb43f6927f3d4f237c872c134b64fc2012313ae
huzaifaghazali <huzaifaghazali@gmail.com>
2024-08-07 13:06:42 +0500

    Add react query in stats page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -72,7 +72,7 @@ const router = createBrowserRouter([
           {
             path: 'stats',
             element: <Stats />,
-            loader: statsLoader,
+            loader: statsLoader(queryClient),
             errorElement: <ErrorElement />,
           },
           {
