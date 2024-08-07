0f403819330bb44de9c4121ba2e5b64bc7850195
huzaifaghazali <huzaifaghazali@gmail.com>
2024-08-07 14:01:59 +0500

    Add react query in edit job page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -94,8 +94,8 @@ const router = createBrowserRouter([
           {
             path: 'edit-job/:id',
             element: <EditJob />,
-            loader: editJobLoader,
-            action: editJobAction,
+            loader: editJobLoader(queryClient),
+            action: editJobAction(queryClient),
           },
           { path: 'delete-job/:id', action: deleteJobAction },
         ],
